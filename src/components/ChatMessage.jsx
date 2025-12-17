5e87db9106d2302d9ef36a78841c2b52a2a10dc6
Amir Malek <amir.malekfarnood@gmail.com>
2025-12-17 23:15:17 +0330

    Added timestamp to each messages.

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,13 +1,17 @@
 import UserProfileImage from '../assets/user.png'
 import RobotProfileImage from '../assets/robot.png'
+import dayjs from 'dayjs'
 import './ChatMessage.css'
 
 export function ChatMessage({ message, sender }) {
+  const timestamp = dayjs().format('h:mma');
+
   return (
-      <div className={sender === 'user' ? 'chat-message-user' : 'chat-message-robot'}>
+      <div className={sender === 'user' ? 'user-chat-message' : 'robot-chat-message'}>
         {sender === 'robot' && <img src={RobotProfileImage} alt="Robot Avatar" className="chat-message-profile" />}
         <div className="chat-message-text">
           {message}
+          <div className={sender === 'user' ? 'user-chat-message-timestamp' : 'robot-chat-message-timestamp'}>{timestamp}</div>
         </div>  
         {sender === 'user' && <img src={UserProfileImage} alt="User Avatar" className="chat-message-profile" />}
       </div>
--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -1,13 +1,17 @@
 import RobotProfileImage from '../assets/robot.png'
+import dayjs from 'dayjs'
 import './WelcomeMessage.css'
 
 export function WelcomeMessage() {
-    return (
-      <div className="initial-message-robot">
-        <img src={RobotProfileImage} alt="Robot Avatar" className="initial-message-profile" />
-        <div className="initial-message-text">
-          Hello! I am your friendly chatbot. How can I assist you today?
-        </div>
+  const timestamp = dayjs().format('h:mma');
+
+  return (
+    <div className="initial-message-robot">
+      <img src={RobotProfileImage} alt="Robot Avatar" className="initial-message-profile" />
+      <div className="initial-message-text">
+        Hello! I am your friendly chatbot. How can I assist you today?
+        <div className="initial-message-timestamp">{timestamp}</div>
       </div>
-    );
-  }
+    </div>
+  );
+}
